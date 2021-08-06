1149db3a8ca39693567bd5990cf9b875117f3991
Irene Lee <69160325+coolirene@users.noreply.github.com>
2021-08-06 19:09:35 -0400

    Add project files.

---
+++ VBookStoreAPI/Migrations/20210806230810_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VBookStoreAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Publishers",
+                columns: table => new
+                {
+                    PublisherId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PublisherName = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publishers", x => x.PublisherId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Publishers");
+        }
+    }
+}
---
+++ VBookStoreAPI/Models/BookStoresDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VBookStoreAPI.Models
+{
+    public class BookStoresDBContext :DbContext
+    {
+        public DbSet<Publisher> Publishers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder
+                .UseSqlServer(@"Data Source=.\SQLEXPRESS;Initial Catalog=BookStoresDB;Integrated Security=True");
+        }
+
+
+    }
+}
